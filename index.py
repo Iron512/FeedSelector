4398ea4c5f80d629393ea1033e29e40c56930668
Iron512 <ivanmartini96@gmail.com>
2020-08-19 13:46:40 +0000

    proceeding with authz

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ task = Flask(__name__)
 
 #os variables
 application_id = str(os.environ.get('APPLICATION_ID'))
-home = str(os.environ.get('APPLICATION_HOME'))
+application_home = str(os.environ.get('APPLICATION_HOME'))
 instagram_app_secret = str(os.environ.get('INSTAGRAM_APP_SECRET'))
 session_secret_key = str(os.environ.get('SESSION_SECRET_KEY'))
 
@@ -22,24 +22,34 @@ def serve():
 		#if no username is logged go to application login
 		auth_url = "https://api.instagram.com/oauth/authorize?client_id="
 		auth_url = auth_url + str(application_id) + "&redirect_uri="
-		auth_url = auth_url + str(home) + "auth/&scope=user_profile,user_media&response_type=code"
+		auth_url = auth_url + str(application_home) + "auth/&scope=user_profile,user_media&response_type=code"
 
 		return redirect(auth_url)
 	
-@task.route('/auth/')
+@task.route('/auth/', methods=['GET'])
 def auth():
 	if request.args.get('code') == None:
 		return "This area is forbidden without a code"
 
-	session['response_code'] = request.args.get('code')
-	
+	params = {
+		"client_id":application_id,
+		"client_secret":instagram_app_secret,
+		"grant_type":"authorization_code"
+		"redirect_uri":str(application_home) + "authtoken/",
+		"code":request.args.get('code')
+	}
+
 	#ask for the short timed user auth
-	response_short_auth = requests.post("https://api.instagram.com/oauth/access_token", data={"data":20})
-	return response_short_auth
+	requests.post("https://api.instagram.com/oauth/access_token", data=params)
 
 	if 'username' not in session:
 		#procede with login
 		return "y"
 
+@task.route('/authtoken/', methods=["POST"])
+def authtoken():
+	return request.form
+
 if __name__ == '__main__':
 	task.run()
+
